060fa425e47207a21094f840aede98054f901868
harris <harris030999gmail.com>
2023-10-29 01:39:28 +0700

    updated

--- emoney/src/main/java/com/emoney/emoney/entity/constants/BaseEntity.java
+++ emoney/src/main/java/com/emoney/emoney/entity/constants/BaseEntity.java
@@ -1,17 +1,29 @@
 package com.emoney.emoney.entity.constants;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
-    @GenericGenerator(strategy = "uuid2", name = "")
+    @GenericGenerator(strategy = "uuid2", name = "system-uuid")
+    @GeneratedValue(generator = "system-uuid")
     private String id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
 }
